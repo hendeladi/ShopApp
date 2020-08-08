1d515b2bae8a009ee12b8b5cd676301231e3c4be
adihendel <hendeladi@gmail.com>
2020-08-08 14:12:48 +0300

    added products

--- app/src/main/java/com/example/shopapp/Services/DataService.kt
+++ app/src/main/java/com/example/shopapp/Services/DataService.kt
@@ -1,6 +1,7 @@
 package com.example.shopapp.Services
 
 import com.example.shopapp.Model.Category
+import com.example.shopapp.Model.Product
 
 object DataService {
     val categories = listOf(
@@ -8,5 +9,27 @@ object DataService {
         Category("HOODIES", "hoodieimage"),
         Category("HATS", "hatomage"),
         Category("DIGITAl", "digitalgoodsimage")
+    )
+
+    val hats = listOf(
+        Product("Graphic Beanie", "18$", "hat01"),
+        Product("Hat Black", "18$", "hat02"),
+        Product( "Hat White", "18$", "hat03"),
+        Product("Hat Snapback", "18$", "hat04")
+    )
+
+    val hoodies = listOf(
+        Product("Hoodie Gray", "25$", "hoodie01"),
+        Product("Hoodie Red", "25$", "hoodie02"),
+        Product("Hoodie White", "25$", "hoodie03"),
+        Product("Hoodie Black", "25$", "hoodie04")
+    )
+
+    val shirts = listOf(
+        Product("Shirt Black", "20$", "shirt01"),
+        Product("Badge Light Gray", "20$", "shirt02"),
+        Product("Logo Shirt Red", "20$", "shirt03"),
+        Product("Hustle", "20$", "shirt04"),
+        Product("Kickflip Studios", "20$", "shirt05")
     )
 }
