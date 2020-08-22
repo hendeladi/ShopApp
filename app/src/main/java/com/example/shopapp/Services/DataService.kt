68454d3fb0adda9228b674eb35d00397739d161f
adi hendel <hendeladi@gmail.com>
2020-08-22 21:51:07 +0300

    added product xml

--- app/src/main/java/com/example/shopapp/Services/DataService.kt
+++ app/src/main/java/com/example/shopapp/Services/DataService.kt
@@ -12,24 +12,35 @@ object DataService {
     )
 
     val hats = listOf(
-        Product("Graphic Beanie", "18$", "hat01"),
-        Product("Hat Black", "18$", "hat02"),
-        Product( "Hat White", "18$", "hat03"),
-        Product("Hat Snapback", "18$", "hat04")
+        Product("Graphic Beanie", "18$", "hat1"),
+        Product("Hat Black", "18$", "hat2"),
+        Product( "Hat White", "18$", "hat3"),
+        Product("Hat Snapback", "18$", "hat4")
     )
 
     val hoodies = listOf(
-        Product("Hoodie Gray", "25$", "hoodie01"),
-        Product("Hoodie Red", "25$", "hoodie02"),
-        Product("Hoodie White", "25$", "hoodie03"),
-        Product("Hoodie Black", "25$", "hoodie04")
+        Product("Hoodie Gray", "25$", "hoodie1"),
+        Product("Hoodie Red", "25$", "hoodie2"),
+        Product("Hoodie White", "25$", "hoodie3"),
+        Product("Hoodie Black", "25$", "hoodie4")
     )
 
     val shirts = listOf(
-        Product("Shirt Black", "20$", "shirt01"),
-        Product("Badge Light Gray", "20$", "shirt02"),
-        Product("Logo Shirt Red", "20$", "shirt03"),
-        Product("Hustle", "20$", "shirt04"),
-        Product("Kickflip Studios", "20$", "shirt05")
+        Product("Shirt Black", "20$", "shirt1"),
+        Product("Badge Light Gray", "20$", "shirt2"),
+        Product("Logo Shirt Red", "20$", "shirt3"),
+        Product("Hustle", "20$", "shirt4"),
+        Product("Kickflip Studios", "20$", "shirt5")
     )
+
+    val digitalGoods = listOf<Product>()
+
+    fun getProduct(category: String): List<Product>{
+        return when(category){
+            "SHIRTS" -> shirts
+            "HATS" -> hats
+            "HOODIES" -> hoodies
+            else -> digitalGoods
+        }
+    }
 }
